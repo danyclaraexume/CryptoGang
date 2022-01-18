5d183b8ba2054e1f77120460f59c0bb61ed26d7c
Exumé, Dany <exume.dany@courrier.uqam.ca>
2022-01-18 15:13:54 -0500

    Menu Links icon added with style adjustment

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -2,15 +2,21 @@ import React from 'react'
 import './Menu.css'
 import {Link} from 'react-router-dom'
 // import SearchIcon from '../../Images/search-icon.png'
+
+import HomeIcon from '../../Images/home-icon.png';
+import WalletIcon from '../../Images/wallet-icon.png'
+import WatchlistIcon from '../../Images/watchlist-icon.png'
+import CalculatorIcon from '../../Images/calculator-icon.png'
+
 const Menu = ({filterValue}) => {
 
     return (
         <div className="MenuContainer">
             <ul>
-                <li><Link to='/' className='menuLink'>Home</Link></li>
-                <li><Link to='/portefolio' className='menuLink'>Portfolio</Link></li>
-                <li><Link to='/watchlist' className='menuLink'>Watchlist</Link></li>
-                <li><Link to='/calculator' className='menuLink'>Calculator</Link></li>
+                <li className='menuLi'><Link to='/' className='menuLink'><img src={HomeIcon} alt='homeIcon' /></Link><label>Home</label></li>
+                <li className='menuLi'><Link to='/portefolio' className='menuLink'><img src={WalletIcon} alt='walletIcon' /></Link><label>Wallet</label></li>
+                <li className='menuLi'><Link to='/watchlist' className='menuLink'><img src={WatchlistIcon} alt='watchlistIcon' /></Link><label>Watchlist</label></li>
+                <li className='menuLi'><Link to='/calculator' className='menuLink'><img src={CalculatorIcon} alt='calculatorIcon' /></Link><label>Calculator</label></li>
             </ul>
             <input type="text" onChange={filterValue} className="searchInput" placeholder="Search coin..."/>
             <button className="loginButton">Login</button>
